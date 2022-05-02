556dbc40d7e6e61dd9f8052c9d87b53f712e3463
Saad Chaay <93975374+saadchaay@users.noreply.github.com>
2022-05-02 23:43:20 +0000

    add logo

--- app/client/src/components/topbar/Topbar.jsx
+++ app/client/src/components/topbar/Topbar.jsx
@@ -1,12 +1,29 @@
 import React from "react";
-import "../../styles/sass/main.css";
+import "../topbar/topbar.css";
+import { NotificationsNone, Language, Settings } from "@material-ui/icons";
 
 export default function Topbar() {
   return (
     <div className="topbar">
       <div className="topbarWrapper">
-        <div className="topLeft">Left</div>
-        <div className="topRight">Right</div>
+        <div className="">
+          <img src="/assets/images/logo.png" alt="" className="logo" />
+        </div>
+        <div className="topRight">
+          <div className="topbarIconContainer">
+            <NotificationsNone />
+            <span className="topIconBadge">2</span>
+          </div>
+          <div className="topbarIconContainer">
+            <Language />
+            <span className="topIconBadge">2</span>
+          </div>
+          <div className="topbarIconContainer">
+            <Settings />
+            <span className="topIconBadge">2</span>
+          </div>
+          <img src="https://images.pexels.com/photos/1526814/pexels-photo-1526814.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="" className="topAvatar" />
+        </div>
       </div>
     </div>
   );
