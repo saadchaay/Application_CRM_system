edd5726f39bf81c6148e3fda8336e46bb3eccd88
Saad Chaay <93975374+saadchaay@users.noreply.github.com>
2022-05-23 11:12:15 +0000

    Create classesValidation.php

---
+++ app/server/helpers/lang/classesValidation.php
@@ -0,0 +1,19 @@
+<?php
+
+    return [
+
+        'controller' => [
+            'required' => 'Controller is required',
+            'alpha' => 'Controller must contain only letters'
+        ],
+        'action' => [
+            'required' => 'Action is required',
+            'alpha' => 'Action must contain only letters'
+        ],
+        'page_title' => [
+            'required' => 'Page title is required'
+        ],
+
+    ];
+
+?>
