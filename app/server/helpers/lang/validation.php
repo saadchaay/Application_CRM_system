3d9243ce458c0e8704e627df094467d9ec4c933b
Saad Chaay <93975374+saadchaay@users.noreply.github.com>
2022-05-23 11:12:27 +0000

    Create validation.php

---
+++ app/server/helpers/lang/validation.php
@@ -0,0 +1,47 @@
+<?php
+
+    return [
+
+
+        'email' => [
+            'required' => 'Email is required',
+            'email' => 'Email is not valid',
+            'unique' => 'Email already exists'
+        ],
+        'username' => [
+            'required' => 'Username is required',
+            'unique' => 'Username already exists'
+        ],
+        'password' => [
+            'required' => 'Password is required',
+            'min' => 'Password must be at least 6 characters',
+            'max' => 'Password must be less than 20 characters',
+            'confirmed' => 'Password confirmation does not match'
+        ],
+        'confirm_password' => [
+            'required' => 'Confirm password is required'
+        ],
+        'name' => [
+            'required' => 'Name is required',
+            'min' => 'Name must be at least 2 characters',
+            'max' => 'Name must be less than 20 characters',
+            'alpha' => 'Name must contain only letters'
+        ],
+        'phone' => [
+            'required' => 'Phone is required',
+            'min' => 'Phone must be at least 10 characters',
+            'max' => 'Phone must be less than 20 characters',
+            'numeric' => 'Phone must contain only numbers'
+        ],
+        'status' => [
+            'required' => 'Status is required'
+        ],
+        'role' => [
+            'required' => 'Role is required'
+        ],
+        'page_title' => [
+            'required' => 'Page title is required'
+        ],
+    ];
+
+?>
