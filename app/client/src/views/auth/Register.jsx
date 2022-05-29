350cc02948161ad1bb7bbe2a6eaf44a0fd5c60ad
Saad Chaay <93975374+saadchaay@users.noreply.github.com>
2022-05-29 16:31:32 +0000

    update all

--- app/client/src/views/auth/Register.jsx
+++ app/client/src/views/auth/Register.jsx
@@ -1,68 +1,307 @@
-import Navbar from "../../components/home/Navbar";
+import { useRef, useState, useEffect, useContext } from "react";
 import logo from "../../assets/images/logo_white_bg.png";
+import AuthContext from "../../context/AuthProvider";
+import axios from "axios";
 
 function Register() {
-    return (
-        <div>
-            <div class=" flex flex-col md:flex-row w-full items-center">
-                <div class="md:h-screen w-auto md:w-1/2 bg-gradient-to-tr bg-sky-800 to-purple-700 i justify-around items-center hidden md:flex">
-                    <div className="flex flex-col justify-center items-center">
-                        <h1 class="text-white font-bold text-4xl font-sans">
-                            <img src={logo} alt="" />
-                        </h1>
-                        <p class="text-white mt-1 w-3/4">Get ready to scale your business and Join Us Now at GROW Your Business.</p>
-                        <button type="submit" class="block w-28 bg-white text-indigo-800 mt-4 py-2 rounded-2xl font-bold mb-2">Read More</button>
-                    </div>
-                </div>
-                <div class="flex w-auto md:w-1/2 justify-center items-center bg-white mt-20 md:mt-0">
-                    <form class="flex flex-col justify-center items-center bg-white md:w-full">
-                        <h1 class="text-gray-800 font-bold text-2xl mb-1">Welcome !</h1>
-                        <p class="text-sm font-normal text-gray-600 mb-7">Register Now</p>
+    const { setAuth } = useContext(AuthContext);
+  const nameRef = useRef();
+  const emailRef = useRef();
+  const usernameRef = useRef();
+  const phoneRef = useRef();
+  const addressRef = useRef();
+  const errRef = useRef();
 
-                        <div class="flex items-center border-2 py-2 px-3 rounded-xl mb-4 w-full md:w-3/4">
-                            <svg class="h-5 w-5 text-gray-400"  width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">  
-                                <path stroke="none" d="M0 0h24v24H0z"/>  <rect x="3" y="5" width="18" height="14" rx="3" />  <line x1="3" y1="10" x2="21" y2="10" />  <line x1="7" y1="15" x2="7.01" y2="15" />  <line x1="11" y1="15" x2="13" y2="15" />
-                            </svg>
-                            <input class="pl-2 outline-none border-none" type="text" name="" placeholder="Full Name" />
-                        </div>
+  const [name, setName] = useState("");
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [pwd, setPwd] = useState("");
+  const [confirmPwd, setConfirmPwd] = useState("");
+  const [phone, setPhone] = useState("");
+  const [address, setAddress] = useState("");
+  const [errMsg, setErrMsg] = useState("");
+  const [success, setSuccess] = useState(false);
 
-                        <div class="flex items-center border-2 py-2 px-3 rounded-xl mb-4 w-full md:w-3/4">
-                            <svg class="h-5 w-5 text-gray-400"  fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z"/>
-                            </svg>
-                            <input class="pl-2 outline-none border-none" type="text" name="" placeholder="Username" />
-                        </div>
+  useEffect(() => {
+    nameRef.current.focus();
+    emailRef.current.focus();
+    usernameRef.current.focus();
+    phoneRef.current.focus();
+    addressRef.current.focus();
+  }, []);
 
-                        <div class="flex items-center border-2 py-2 px-3 rounded-xl mb-4 w-full md:w-3/4">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207" />
-                            </svg>
-                            <input class="pl-2 outline-none border-none" type="email" name="" placeholder="Email Address" />
-                        </div>
+  useEffect(() => {
+    setErrMsg("");
+  }, [username, pwd]);
 
-                        <div class="flex items-center border-2 py-2 px-3 rounded-xl mb-4 w-full md:w-3/4">
-                            <svg class="h-5 w-5 text-gray-400"  width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">  
-                                <path stroke="none" d="M0 0h24v24H0z"/>  
-                                <path d="M5 4h4l2 5l-2.5 1.5a11 11 0 0 0 5 5l1.5 -2.5l5 2v4a2 2 0 0 1 -2 2a16 16 0 0 1 -15 -15a2 2 0 0 1 2 -2" />
-                            </svg>
-                            <input class="pl-2 outline-none border-none" type="text" name="" placeholder="Phone Number" />
-                        </div>
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log(name, username, email, pwd, confirmPwd, phone, address);
+    const res = await axios.get("https://api.github.com/users/octocat/followers", {});
+    console.log(res);
+    setName("");
+    setUsername("");
+    setEmail("");
+    setPwd("");
+    setConfirmPwd("");
+    setPhone("");
+    setAddress("");
 
-                        <div class="flex items-center border-2 py-2 px-3 rounded-xl w-full md:w-3/4">
-                            <svg class="h-5 w-5 text-gray-400"  fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"/>
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"/>
-                            </svg>
-                            <input class="pl-2 outline-none border-none" type="text" name="" placeholder="Address" />
-                        </div>
-                        
-                        <button type="submit" class="block w-full bg-sky-800 mt-4 py-2 rounded-xl text-white font-semibold mb-2 w-2/3 md:w-1/4">Submit</button>
-                        <span class="text-sm ml-2 hover:text-blue-500 cursor-pointer">Already have an account ?</span>
-                    </form>
-                </div>
-                </div>
+    setSuccess(true);
+  };
+
+  return (
+    <div>
+      <div className=" flex flex-col md:flex-row w-full items-center">
+        <div className="md:h-screen w-auto md:w-1/2 bg-gradient-to-tr bg-sky-800 to-purple-700 i justify-around items-center hidden md:flex">
+          <div className="flex flex-col justify-center items-center">
+            <h1 className="text-white font-bold text-4xl font-sans">
+              <img src={logo} alt="" />
+            </h1>
+            <p className="text-white mt-1 w-3/4">
+              Get ready to scale your business and Join Us Now at GROW Your
+              Business.
+            </p>
+            <button
+              type="submit"
+              className="block w-28 bg-white text-indigo-800 mt-4 py-2 rounded-2xl font-bold mb-2"
+            >
+              Read More
+            </button>
+          </div>
+        </div>
+        <div className="flex w-auto md:w-1/2 justify-center items-center bg-white mt-20 md:mt-0">
+          <form
+            className="flex flex-col justify-center items-center bg-white w-80 md:w-full"
+            onSubmit={handleSubmit}
+          >
+            <h1 className="text-gray-800 font-bold text-2xl mb-1">Welcome !</h1>
+            <p className="text-sm font-normal text-gray-600 mb-7">Register Now</p>
+
+            <div className="flex items-center border-2 py-2 px-3 rounded-xl mb-4 w-full md:w-3/4">
+              <svg
+                className="h-5 w-5 text-gray-400"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                stroke-width="2"
+                stroke="currentColor"
+                fill="none"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              >
+                <path stroke="none" d="M0 0h24v24H0z" />{" "}
+                <rect x="3" y="5" width="18" height="14" rx="3" />{" "}
+                <line x1="3" y1="10" x2="21" y2="10" />{" "}
+                <line x1="7" y1="15" x2="7.01" y2="15" />{" "}
+                <line x1="11" y1="15" x2="13" y2="15" />
+              </svg>
+              <input
+                ref={nameRef}
+                className="pl-2 outline-none border-none"
+                type="text"
+                autoComplete="off"
+                onChange={(e) => setName(e.target.value)}
+                id="name"
+                value={name}
+                placeholder="Full Name"
+              />
             </div>
-    );
+            <div className="text-red-500 mb-3 text-sm">
+              {errMsg ? errMsg : null}
+            </div>
+
+
+            <div className="flex items-center border-2 py-2 px-3 rounded-xl mb-4 w-full md:w-3/4">
+              <svg
+                className="h-5 w-5 text-gray-400"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+                />
+              </svg>
+              <input
+                ref={usernameRef}
+                className="pl-2 outline-none border-none"
+                type="text"
+                autoComplete="off"
+                onChange={(e) => setUsername(e.target.value)}
+                id="username"
+                value={username}
+                placeholder="Username"
+              />
+            </div>
+            <div className="text-red-500 mb-3 text-sm">
+              Phone number is not valid
+            </div>
+
+            <div className="flex items-center border-2 py-2 px-3 rounded-xl mb-4 w-full md:w-3/4">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5 text-gray-400"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207"
+                />
+              </svg>
+              <input
+                ref={emailRef}
+                className="pl-2 outline-none border-none"
+                type="email"
+                id="email"
+                autoComplete="off"
+                onChange={(e) => setEmail(e.target.value)}
+                value={email}
+                placeholder="Email Address"
+              />
+            </div>
+            <div className="text-red-500 mb-3 text-sm">Email is already taken</div>
+
+            <div className="flex items-center border-2 py-2 px-3 rounded-xl mb-4 w-full md:w-3/4">
+              <svg
+                className="h-5 w-5 text-gray-400"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                stroke-width="2"
+                stroke="currentColor"
+                fill="none"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              >
+                <path stroke="none" d="M0 0h24v24H0z" />
+                <path d="M5 4h4l2 5l-2.5 1.5a11 11 0 0 0 5 5l1.5 -2.5l5 2v4a2 2 0 0 1 -2 2a16 16 0 0 1 -15 -15a2 2 0 0 1 2 -2" />
+              </svg>
+              <input
+                ref={phoneRef}
+                className="pl-2 outline-none border-none"
+                type="text"
+                id="phone"
+                autoComplete="off"
+                onChange={(e) => setPhone(e.target.value)}
+                value={phone}
+                placeholder="Phone Number"
+              />
+            </div>
+            <div className="text-red-500 mb-3 text-sm">
+              Phone number is not valid
+            </div>
+
+            <div className="flex items-center border-2 py-2 px-3 rounded-xl w-full md:w-3/4">
+              <svg
+                className="h-5 w-5 text-gray-400"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"
+                />
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"
+                />
+              </svg>
+              <input
+                ref={addressRef}
+                className="pl-2 outline-none border-none"
+                type="text"
+                id="address"
+                autoComplete="off"
+                onChange={(e) => setAddress(e.target.value)}
+                value={address}
+                placeholder="Address"
+              />
+            </div>
+            <div className="text-red-500 mb-3 text-sm">
+              Phone number is not valid
+            </div>
+
+            <div className="flex items-center border-2 py-2 px-3 rounded-xl w-full md:w-3/4 mt-4">
+              <svg
+                className="h-5 w-5 text-gray-400"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z"
+                />
+              </svg>
+              <input
+                className="pl-2 outline-none border-none"
+                type="password"
+                id="password"
+                onChange={(e) => setPwd(e.target.value)}
+                value={pwd}
+                placeholder="Password"
+              />
+            </div>
+            <div className="text-red-500 mb-3 text-sm">
+              Phone number is not valid
+            </div>
+
+            <div className="flex items-center border-2 py-2 px-3 rounded-xl w-full md:w-3/4 mt-4">
+              <svg
+                className="h-5 w-5 text-gray-400"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z"
+                />
+              </svg>
+              <input
+                className="pl-2 outline-none border-none"
+                type="password"
+                id="confirmPassword"
+                onChange={(e) => setConfirmPwd(e.target.value)}
+                value={confirmPwd}
+                placeholder="Confirm password"
+              />
+            </div>
+            <div className="text-red-500 mb-3 text-sm">
+              Phone number is not valid
+            </div>
+
+            <button
+              type="submit"
+              className="block w-full bg-sky-800 mt-4 py-2 rounded-xl text-white font-semibold mb-2 w-2/3 md:w-1/4"
+            >
+              Submit
+            </button>
+            <span className="text-sm ml-2 hover:text-blue-500 cursor-pointer">
+              Already have an account ?
+            </span>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Register;
--- app/server/index.php
+++ app/server/index.php
@@ -35,75 +35,22 @@ if(isset($params[0]) & !empty($params[0])){
                         }
                     } else {
                         http_response_code(404);
-                        echo "<h3>This method doesn't exist</h3>";
-                        // echo json_encode(array("message" => $controllerValidation['action']['required']));
+                        echo json_encode(array("message" => "This method doesn't exist"));
                     }  
                 } else {
                     http_response_code(404);
-                    echo "<h3>This method doesn't exist</h3>";
-                    // echo json_encode(array("message" => $controllerValidation['action']['required']));
+                    echo json_encode(array("message" => "This method doesn't exist"));
                 }
             } else {
                 http_response_code(404);
-                echo "<h3>This class doesn't exist</h3>";
-                // echo json_encode(array("message" => $controllerValidation['controller']['required']));
+                echo json_encode(array("message" => "This class doesn't exist"));
             }
         } else {
             http_response_code(404);
-            echo "<h3>This file doesn't exist</h3>";
-            // echo json_encode(array("message" => $controllerValidation['file']['required']));
+            echo json_encode(array("message" => "This file doesn't exist"));
         }
     }
 }
 
-/* if(isset($params[0]) & !empty($params[0])){
-    // 
-    if($params[0] == 'auth'){
-        print_r($params);
-        array_shift($params);
-        var_dump($params);
-        $controller = ucfirst($params[0]);
-        $file = 'controllers/auth/' . $controller . '.php';
-        if(file_exists($file)){
-
-            require_once $file;
-    
-            if(class_exists($controller)){
-                $obj = new $controller();
-                
-                if(isset($params[1]) & !empty($params[1])){
-                    $action = $params[1];
-    
-                    if(method_exists($obj, $action)){
-                        if(isset($params[2]) & !empty($params[2])){
-                            $obj->$action($params[2]);
-                        } else {
-                            $obj->$action();
-                        }
-                    } else {
-                        http_response_code(404);
-                        echo "<h3>This method doesn't exist</h3>";
-                        // echo json_encode(array("message" => $controllerValidation['action']['required']));
-                    }  
-                } else {
-                    http_response_code(404);
-                    echo "<h3>This method doesn't exist</h3>";
-                    // echo json_encode(array("message" => $controllerValidation['action']['required']));
-                }
-            } else {
-                http_response_code(404);
-                echo "<h3>This class doesn't exist</h3>";
-                // echo json_encode(array("message" => $controllerValidation['controller']['required']));
-            }
-        } else {
-            http_response_code(404);
-            echo "<h3>This file doesn't exist</h3>";
-            // echo json_encode(array("message" => $controllerValidation['file']['required']));
-        }
-    }
-    
-
-    
-} */
 
 ?>
