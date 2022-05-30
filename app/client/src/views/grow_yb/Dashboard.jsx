69baeb7430892a69be5a7994268187450ba44773
Saad Chaay <93975374+saadchaay@users.noreply.github.com>
2022-05-30 20:56:56 +0000

    Update Dashboard.jsx

--- app/client/src/views/grow_yb/Dashboard.jsx
+++ app/client/src/views/grow_yb/Dashboard.jsx
@@ -547,13 +547,13 @@ export default function Example() {
                 >
                   <div className="flex-1 flex justify-between">
                     <a
-                      href="#"
+                      href="/"
                       className="relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:text-gray-500"
                     >
                       Previous
                     </a>
                     <a
-                      href="#"
+                      href="/"
                       className="ml-3 relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:text-gray-500"
                     >
                       Next
