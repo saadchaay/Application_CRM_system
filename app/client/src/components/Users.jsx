d57173275b15bb9f81923098cbfd0739f9f614cb
Saad Chaay <93975374+saadchaay@users.noreply.github.com>
2022-06-02 11:41:31 +0000

    Update Users.jsx

--- app/client/src/components/Users.jsx
+++ app/client/src/components/Users.jsx
@@ -1,45 +1,12 @@
-import { ScaleIcon } from "@heroicons/react/outline";
-import {
-  CashIcon,
-  CheckCircleIcon,
-  ChevronRightIcon,
-  OfficeBuildingIcon,
-} from "@heroicons/react/solid";
 
-const cards = [
-  { name: "Account balance", href: "#", icon: ScaleIcon, amount: "$30,659.45" },
-  // More items...
-];
 
-const transactions = [
-  {
-    id: 1,
-    name: "Payment to Molly Sanders",
-    href: "#",
-    amount: "$20,000",
-    currency: "USD",
-    status: "success",
-    date: "July 11, 2020",
-    datetime: "2020-07-11",
-  },
-  // More transactions...
-];
-const statusStyles = {
-  success: "bg-green-100 text-green-800",
-  processing: "bg-yellow-100 text-yellow-800",
-  failed: "bg-gray-100 text-gray-800",
-};
-
-function classNames(...classes) {
-  return classes.filter(Boolean).join(" ");
-}
-
-export default function Main() {
+export default function Users() {
   return (
     <>
-      <main className="flex-1 pb-8">
-        {/* Page header */}
-        This is user here
+      <main className="flex justify-center pb-8">
+          <h1 className="text-xl font-bold">
+            This is user here
+          </h1>
       </main>
     </>
   );
