a6e0db0f1fa483489bb3a32a36f42f3999456bee
Saad Chaay <93975374+saadchaay@users.noreply.github.com>
2022-06-02 21:29:19 +0000

    Update Dashboard.jsx

--- app/client/src/components/system/Dashboard.jsx
+++ app/client/src/components/system/Dashboard.jsx
@@ -66,7 +66,7 @@ export default function Dashboard() {
   ]);
 
   const people = [
-    { name: 'Lindsay Walton', title: 'Front-end Developer', email: 'lindsay.walton@example.com', role: 'Member' },
+    { Number: '#5627', date: '28-05-2022', customer: 'Saad Chaay', items: 'Product 1, Product 2', status: 'Pending', total: '$320' },
   ]
 
   return (
@@ -194,66 +194,71 @@ export default function Dashboard() {
                       scope="col"
                       className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6"
                     >
-                      Customer
+                      Number
+                    </th>
+                    <th
+                      scope="col"
+                      className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6"
+                    >
+                      Date
                     </th>
                     <th
                       scope="col"
                       className="hidden px-3 py-3.5 text-left text-sm font-semibold text-gray-900 lg:table-cell"
                     >
-                      Products
+                      Customer
                     </th>
                     <th
                       scope="col"
                       className="hidden px-3 py-3.5 text-left text-sm font-semibold text-gray-900 sm:table-cell"
                     >
-                      Email
+                      Items
                     </th>
                     <th
                       scope="col"
                       className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
                     >
-                      Role
+                      Status
                     </th>
                     <th
                       scope="col"
-                      className="relative py-3.5 pl-3 pr-4 sm:pr-6"
+                      className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
                     >
-                      <span className="sr-only">Edit</span>
+                      Total
                     </th>
+                    
                   </tr>
                 </thead>
                 <tbody className="divide-y divide-gray-200 bg-white">
                   {people.map((person) => (
                     <tr key={person.email}>
                       <td className="w-full max-w-0 py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:w-auto sm:max-w-none sm:pl-6">
-                        {person.name}
+                        {person.Number}
                         <dl className="font-normal lg:hidden">
-                          <dt className="sr-only">Title</dt>
+                          <dt className="sr-only">Date</dt>
                           <dd className="mt-1 truncate text-gray-700">
-                            {person.title}
+                            {person.date}
                           </dd>
-                          <dt className="sr-only sm:hidden">Email</dt>
+                          <dt className="sr-only sm:hidden">Customer</dt>
                           <dd className="mt-1 truncate text-gray-500 sm:hidden">
-                            {person.email}
+                            {person.customer}
                           </dd>
                         </dl>
                       </td>
                       <td className="hidden px-3 py-4 text-sm text-gray-500 lg:table-cell">
-                        {person.title}
+                        {person.date}
                       </td>
                       <td className="hidden px-3 py-4 text-sm text-gray-500 sm:table-cell">
-                        {person.email}
+                        {person.customer}
                       </td>
                       <td className="px-3 py-4 text-sm text-gray-500">
-                        {person.role}
+                        {person.items}
                       </td>
-                      <td className="py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6">
-                        <a
-                          href="#"
-                          className="text-indigo-600 hover:text-indigo-900"
-                        >
-                          Edit<span className="sr-only">, {person.name}</span>
-                        </a>
+                      <td className="px-3 py-4 text-sm text-gray-500">
+                        {person.status}
+                      </td>
+                      <td className="px-3 py-4 text-sm text-gray-500">
+                        {person.total}
                       </td>
                     </tr>
                   ))}
