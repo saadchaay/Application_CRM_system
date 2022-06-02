d65acf81ad6914420c605e909da18dfcbea60ad8
Saad Chaay <93975374+saadchaay@users.noreply.github.com>
2022-06-02 22:41:49 +0000

    Update Profile.jsx

--- app/client/src/components/system/account/Profile.jsx
+++ app/client/src/components/system/account/Profile.jsx
@@ -1,100 +1,13 @@
-/*
-  This example requires Tailwind CSS v2.0+ 
-  
-  This example requires some changes to your config:
-  
-  ```
-  // tailwind.config.js
-  const colors = require('tailwindcss/colors')
-  
-  module.exports = {
-    // ...
-    theme: {
-      extend: {
-        colors: {
-          orange: colors.orange,
-        },
-      },
-    },
-    plugins: [
-      // ...
-      require('@tailwindcss/forms'),
-    ],
-  }
-  ```
-*/
-import { Fragment, useState } from "react";
 import {
-  Disclosure,
-  Menu,
-  RadioGroup,
-  Switch,
-  Transition,
-} from "@headlessui/react";
-import { QuestionMarkCircleIcon, SearchIcon } from "@heroicons/react/solid";
-import {
-  BellIcon,
   CogIcon,
-  CreditCardIcon,
   KeyIcon,
-  MenuIcon,
   UserCircleIcon,
-  ViewGridAddIcon,
-  XIcon,
 } from "@heroicons/react/outline";
 
-const user = {
-  name: "Lisa Marie",
-  email: "lisamarie@example.com",
-  imageUrl:
-    "https://images.unsplash.com/photo-1517365830460-955ce3ccd263?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=80",
-};
-const navigation = [
-  { name: "Dashboard", href: "#" },
-  { name: "Jobs", href: "#" },
-  { name: "Applicants", href: "#" },
-  { name: "Company", href: "#" },
-];
-const userNavigation = [
-  { name: "Your Profile", href: "#" },
-  { name: "Settings", href: "#" },
-  { name: "Sign out", href: "#" },
-];
 const subNavigation = [
   { name: "Profile", href: "#", icon: UserCircleIcon, current: false },
   { name: "Settings", href: "#", icon: CogIcon, current: true },
   { name: "Password", href: "#", icon: KeyIcon, current: false },
-];
-const plans = [
-  {
-    name: "Startup",
-    priceMonthly: 29,
-    priceYearly: 290,
-    limit: "Up to 5 active job postings",
-  },
-  {
-    name: "Business",
-    priceMonthly: 99,
-    priceYearly: 990,
-    limit: "Up to 25 active job postings",
-  },
-  {
-    name: "Enterprise",
-    priceMonthly: 249,
-    priceYearly: 2490,
-    limit: "Unlimited active job postings",
-  },
-];
-const payments = [
-  {
-    id: 1,
-    date: "1/1/2020",
-    datetime: "2020-01-01",
-    description: "Business Plan - Annual Billing",
-    amount: "CA$109.00",
-    href: "#",
-  },
-  // More payments...
 ];
 
 function classNames(...classes) {
@@ -102,19 +15,10 @@ function classNames(...classes) {
 }
 
 export default function Example() {
-  const [selectedPlan, setSelectedPlan] = useState(plans[1]);
-  const [annualBillingEnabled, setAnnualBillingEnabled] = useState(true);
+  
 
   return (
     <>
-      {/*
-        This example requires updating your template:
-
-        ```
-        <html class="h-full bg-gray-100">
-        <body class="h-full">
-        ```
-      */}
       <div className="h-full">
         <main className="max-w-7xl mx-auto pb-10 lg:py-2 lg:px-8">
           <div className="flex flex-col justify-between items-start">
@@ -123,6 +27,7 @@ export default function Example() {
                 Account
               </h1>
             </div>
+
             {/* <aside className="py-6 px-2 sm:px-6 lg:py-0 lg:px-0 lg:col-span-3"> */}
             <nav className="flex mt-5 border-y py-4 w-full">
               {subNavigation.map((item) => (
