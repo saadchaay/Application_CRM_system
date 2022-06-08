896f9571c4c2281d8f6e992f88581cc52cb4445c
Saad Chaay <93975374+saadchaay@users.noreply.github.com>
2022-06-08 15:46:20 +0000

    delete comments

--- app/client/src/components/helpers/EditCategory.jsx
+++ app/client/src/components/helpers/EditCategory.jsx
@@ -26,6 +26,7 @@ const EditCategory = ({item, handleCancel, handleEditChange}) => {
           autoComplete="title"
           className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-gray-900 focus:border-gray-900 sm:text-sm"
         />
+        {/* {errors.title ? <p className="text-red-500 text-xs italic">{errors.title}</p> : null} */}
       </td>
       <td className="px-3 py-4 text-sm text-gray-500">
         <textarea
@@ -36,6 +37,7 @@ const EditCategory = ({item, handleCancel, handleEditChange}) => {
           autoComplete="description"
           className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-gray-900 focus:border-gray-900 sm:text-sm"
         />
+        {/* {errors.description ? <p className="text-red-500 text-xs italic">{errors.description}</p> : null} */}
       </td>
       <td className="py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6">
         <button
--- app/client/src/components/system/Categories.jsx
+++ app/client/src/components/system/Categories.jsx
@@ -8,6 +8,7 @@ import HandleCategory from "../helpers/HandleCategory";
 export default function Example() {
   const [categories, setCategories] = useState([]);
   const [editItemId, setEditItemId] = useState(null);
+  const [errors, setErrors] = useState();
   const [open, setOpen] = useState(false);
   const cancelButtonRef = useRef(null);
   const auth = JSON.parse(localStorage.getItem("auth"));
@@ -49,15 +50,6 @@ export default function Example() {
   const handleClick = async (event, item) => {
     event.preventDefault();
     setEditItemId(item.id);
-
-    // const formValues = {
-    //   id_creator: auth.id,
-    //   type: auth.role === "admin" ? "admin" : "user",
-    //   title: item.title,
-    //   description: item.description,
-    // };
-
-    // setEditData(formValues);
   };
   const handleCancel = (event) => {
     event.preventDefault();
@@ -109,8 +101,6 @@ export default function Example() {
   };
   const handleUpdate = async (e) => {
     e.preventDefault();
-    console.log(editData);
-    console.log(editItemId);
     const res = await axios.put(
       "CategoriesController/update/" + editItemId,
       JSON.stringify(editData),
@@ -127,6 +117,7 @@ export default function Example() {
       console.log("Category updated");
       setEditItemId(null);
     } else {
+      setErrors(res.data);
       console.log(res.data);
     }
   };
@@ -315,51 +306,13 @@ export default function Example() {
               </thead>
               <tbody className="divide-y divide-gray-200 bg-white">
                 {categories.map((item) => (
-                  //   <tr key={item.id}>
-                  //   <td className="max-w-0 py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:w-auto sm:max-w-none sm:pl-6">
-                  //     ## {item.id}
-                  //     <dl className="font-normal lg:hidden">
-                  //       <dt className="sr-only">Title</dt>
-                  //       <dd className="mt-1 truncate text-gray-700">
-                  //         {item.title}
-                  //       </dd>
-                  //       <dt className="sr-only sm:hidden">Created at</dt>
-                  //       <dd className="mt-1 truncate text-gray-500 sm:hidden">
-                  //         {item.created_at}
-                  //       </dd>
-                  //     </dl>
-                  //   </td>
-                  //   <td className="hidden px-3 py-4 text-sm text-gray-500 lg:table-cell">
-                  //     {item.created_at}
-                  //   </td>
-                  //   <td className="hidden px-3 py-4 text-sm text-gray-500 sm:table-cell">
-                  //     {item.title}
-                  //   </td>
-                  //   <td className="px-3 py-4 text-sm text-gray-500">
-                  //     {item.description}
-                  //   </td>
-                  //   <td className="py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6">
-                  //     <button
-                  //       onClick={() => displayModalUpdate(item.id)}
-                  //       className="text-green-500 hover:text-green-700"
-                  //     >
-                  //      <Edit />
-                  //     </button>
-                  //     <button
-                  //       onClick={() => handleDelete(item.id)}
-                  //       className="text-red-500 hover:text-red-600"
-                  //     >
-                  //       <Delete />
-                  //     </button>
-                  //   </td>
-
-                  // </tr>
                   <Fragment>
                     {editItemId === item.id ? (
                       <EditCategory
                         item={item}
                         handleCancel={handleCancel}
                         handleEditChange={handleEditChange}
+                        // errors={errors}
                       />
                     ) : (
                       <HandleCategory
@@ -369,7 +322,6 @@ export default function Example() {
                       />
                     )}
 
-                    {/* <HandleCategory item={item} />  */}
                   </Fragment>
                 ))}
               </tbody>
