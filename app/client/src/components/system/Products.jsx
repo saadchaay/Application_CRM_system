943dfc69ffbfc4725df957a3d1159feb80973a40
Saad Chaay <93975374+saadchaay@users.noreply.github.com>
2022-06-08 22:58:07 +0000

    Update Products.jsx

--- app/client/src/components/system/Products.jsx
+++ app/client/src/components/system/Products.jsx
@@ -34,7 +34,6 @@ export default function Example() {
     avatar: "",
   });
 
-
   // fetch all products
   const fetchProducts = async () => {
     const type = auth.role === "admin" ? "admin" : "user";
@@ -71,18 +70,15 @@ export default function Example() {
       }
       return product;
     });
-    
+
     setProducts(newProducts);
 
     const id = product[index].id;
-    const res = await axios.put(
-      "ProductsController/changeStatus/" +id,
-      {
-        headers: {
-          "Content-Type": "application/json",
-        },
-      }
-    );
+    const res = await axios.put("ProductsController/changeStatus/" + id, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
 
     if (res.data.status === "success") {
       console.log(res.data.status);
@@ -134,30 +130,57 @@ export default function Example() {
                             as="h3"
                             className="text-lg leading-6 font-bold text-gray-900"
                           >
-                            Add Category
+                            Add User
                           </Dialog.Title>
                           <div className="mt-2">
-                            <div className="mt-3 flex flex-col w-80">
-                              <div className="col-span-4 sm:col-span-12">
+                            <div className="mt-3 grid grid-cols-4 gap-6">
+                              <div className="col-span-4 sm:col-span-2">
                                 <label
                                   htmlFor="first-name"
                                   className="block text-sm font-medium text-gray-700"
                                 >
-                                  Title
+                                  Product name
                                 </label>
                                 <input
                                   type="text"
                                   id="title"
-                                  // value={title}
-                                  // onChange={(e) => setTitle(e.target.value)}
+                                  // ref={nameRef}
+                                  // value={name}
+                                  // onChange={(e) => setName(e.target.value)}
                                   autoComplete="title"
                                   className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-gray-900 focus:border-gray-900 sm:text-sm"
                                 />
                                 <div className="text-red-500 mb-3 text-sm">
-                                  {/* {errTitle ? errTitle : null} */}
+                                  {/* {errName ? errName : null} */}
                                 </div>
                               </div>
-                              <div className="col-span-4 sm:col-span-12">
+                              <div className="col-span-4 sm:col-span-2">
+                                <label
+                                  htmlFor="last-name"
+                                  className="block text-sm font-medium text-gray-700"
+                                >
+                                  Category
+                                </label>
+                                <select
+                                  id="role"
+                                  autoComplete="country-name"
+                                  className="mt-1 block w-full bg-white border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-gray-900 focus:border-gray-900 sm:text-sm"
+                                >
+                                  <option>Choose category ...</option>
+                                  {categories.map((category) => (
+                                    <option
+                                      key={category.id}
+                                      value={category.id}
+                                    >
+                                      {category.name}
+                                    </option>
+                                  ))}
+                                </select>
+                              </div>
+                            </div>
+
+                            <div className="mt-3 grid grid-cols-2 gap-6">
+                              <div className="col-span-4 sm:col-span-2">
                                 <label
                                   htmlFor="last-name"
                                   className="block text-sm font-medium text-gray-700"
@@ -167,15 +190,98 @@ export default function Example() {
                                 <textarea
                                   type="text"
                                   id="description"
-                                  // value={description}
-                                  // onChange={(e) =>
-                                  // setDescription(e.target.value)
-                                  // }
                                   autoComplete="description"
                                   className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-gray-900 focus:border-gray-900 sm:text-sm"
                                 />
-                                <div className="text-red-500 mb-3 text-sm">
-                                  {/* {errDescription ? errDescription : null} */}
+                              </div>
+                            </div>
+
+                            <div className="mt-3 grid grid-cols-4 gap-6">
+                              <div className="col-span-4 sm:col-span-2">
+                                <label
+                                  htmlFor="first-name"
+                                  className="block text-sm font-medium text-gray-700"
+                                >
+                                  Quantity
+                                </label>
+                                <input
+                                  type="text"
+                                  id="quantity"
+                                  autoComplete="quantity"
+                                  className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-gray-900 focus:border-gray-900 sm:text-sm"
+                                />
+                              </div>
+                              <div className="col-span-4 sm:col-span-2">
+                                <label
+                                  htmlFor="last-name"
+                                  className="block text-sm font-medium text-gray-700"
+                                >
+                                  Price
+                                </label>
+                                <input
+                                  type="text"
+                                  id="price"
+                                  autoComplete="price"
+                                  className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-gray-900 focus:border-gray-900 sm:text-sm"
+                                />
+                              </div>
+                            </div>
+
+                            {/* <div className="mt-3 grid grid-cols-4 gap-6">
+                              <div className="col-span-4 sm:col-span-2">
+                                <label
+                                  htmlFor="country"
+                                  className="block text-sm font-medium text-gray-700"
+                                >
+                                  Picture
+                                </label>
+                                <input
+                                  type="file"
+                                  id="picture"
+                                  className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-gray-900 focus:border-gray-900 sm:text-sm"
+                                />
+
+                              </div>
+                            </div> */}
+
+                            <div>
+                              <label className="block text-sm font-medium text-gray-700">
+                                Cover photo
+                              </label>
+                              <div className="mt-1 flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md">
+                                <div className="space-y-1 text-center">
+                                  <svg
+                                    className="mx-auto h-12 w-12 text-gray-400"
+                                    stroke="currentColor"
+                                    fill="none"
+                                    viewBox="0 0 48 48"
+                                    aria-hidden="true"
+                                  >
+                                    <path
+                                      d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
+                                      strokeWidth={2}
+                                      strokeLinecap="round"
+                                      strokeLinejoin="round"
+                                    />
+                                  </svg>
+                                  <div className="flex text-sm text-gray-600">
+                                    <label
+                                      htmlFor="file-upload"
+                                      className="relative cursor-pointer bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500"
+                                    >
+                                      <span>Upload a file</span>
+                                      <input
+                                        id="file-upload"
+                                        name="file-upload"
+                                        type="file"
+                                        className="sr-only"
+                                      />
+                                    </label>
+                                    <p className="pl-1">or drag and drop</p>
+                                  </div>
+                                  <p className="text-xs text-gray-500">
+                                    PNG, JPG, GIF up to 10MB
+                                  </p>
                                 </div>
                               </div>
                             </div>
