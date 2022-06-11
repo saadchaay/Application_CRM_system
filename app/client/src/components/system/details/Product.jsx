48917fe02946488837407ba60495fbe5cb49dc12
Saad Chaay <93975374+saadchaay@users.noreply.github.com>
2022-06-11 10:58:34 +0000

    Update Product.jsx

--- app/client/src/components/system/details/Product.jsx
+++ app/client/src/components/system/details/Product.jsx
@@ -1,17 +1,31 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import axios from "../../../api/axios";
 
 
 export default function Product() {
     const [product, setProduct] = useState(null);
-    const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
-    const [success, setSuccess] = useState(null);
     const { id } = useParams();
-    
+
+    const fetchProduct = async () => {
+        const res = await axios.get(`ProductsController/show/${id}`);
+        if(res.data.success) {
+            setProduct(res.data);
+            console.log(res.data);
+        } else {
+            setError(res);
+            console.log(res);
+        }
+    }
+
+    useEffect(() => {
+        fetchProduct();
+    })
+
     return(
         <div>
-            {id}
+            {product.title}
         </div>
     );
 };
