a53b4fc77f765256043d771bb902f91115fb9f1c
Saad Chaay <93975374+saadchaay@users.noreply.github.com>
2022-06-11 21:54:54 +0000

    Update Product.jsx

--- app/client/src/components/system/details/Product.jsx
+++ app/client/src/components/system/details/Product.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { useParams, Navigate } from "react-router-dom";
-import { Cancel, SaveAlt, Edit } from "@material-ui/icons";
+import { Cancel, SaveAlt, Edit, Delete } from "@material-ui/icons";
 import axios from "../../../api/axios";
 import Drop from "../../helpers/Drop";
 
