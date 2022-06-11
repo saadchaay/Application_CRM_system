031eb5aadc28db7947e13cfb002014fbcf82ec70
Saad Chaay <93975374+saadchaay@users.noreply.github.com>
2022-06-11 10:19:51 +0000

    Update Products.jsx

--- app/client/src/components/system/Products.jsx
+++ app/client/src/components/system/Products.jsx
@@ -281,7 +281,7 @@ export default function Example() {
                                     autoComplete="country-name"
                                     className="mt-1 block w-full bg-white border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-gray-900 focus:border-gray-900 sm:text-sm"
                                   >
-                                    <option>Choose category ...</option>
+                                    <option value="">Choose category ...</option>
                                     {categories.map((category) => (
                                       <option
                                         key={category.id}
