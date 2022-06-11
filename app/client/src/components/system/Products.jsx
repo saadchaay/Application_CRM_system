8d1b649b0e37e0ee80754921672145a4ad18e82b
Saad Chaay <93975374+saadchaay@users.noreply.github.com>
2022-06-11 10:51:33 +0000

    Update Products.jsx

--- app/client/src/components/system/Products.jsx
+++ app/client/src/components/system/Products.jsx
@@ -567,7 +567,7 @@ export default function Example() {
                 {products.map((item, index) => (
                   <tr key={item.id}>
                     <td className="w-full max-w-0 py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:w-auto sm:max-w-none sm:pl-6">
-                      <Link to={`/products/${item.id}`} > ## {item.id} </Link> 
+                      <Link to={`/product/${item.id}`} > ## {item.id} </Link> 
                       <dl className="font-normal lg:hidden">
                         <dt className="sr-only">Category</dt>
                         <dd className="mt-1 truncate text-gray-700">
