1c053af43f88f8436f2d3ceeda983e9db6a68def
Saad Chaay <93975374+saadchaay@users.noreply.github.com>
2022-06-13 23:11:36 +0100

    Update Integration.jsx

--- app/client/src/components/system/Integration.jsx
+++ app/client/src/components/system/Integration.jsx
@@ -1,21 +1,28 @@
-
-
-
 export default function Integration() {
-
-    return (
-        <>
-            <div className="bg-white shadow">
-          <div className="px-4 sm:px-6 lg:max-w-6xl lg:mx-auto lg:px-8">
-            <div className="py-6 md:flex md:items-center md:justify-between lg:border-t lg:border-gray-200">
-              <div className="flex-1 min-w-0">
-
-                menu s
-                    
-              </div>
+  return (
+    <>
+      <div className="flex-1 min-w-0 lg:border-t pt-4 ml-5">
+        <div className="bg-white rounded-3xl border shadow-xl p-8 w-60 sm:w-3/6">
+          <div className="flex justify-between items-center mb-4">
+            <img
+              src="https://cdn.neow.in/news/images/uploaded/2019/10/1570089797_google-sheets_story.jpg"
+              alt=""
+              className="w-32"
+            />
+            <div>
+              <span className="font-bold text-green-500">+ $726 (15%)</span>
+              <br />
+              <span className="font-medium text-xs text-gray-500 flex justify-end">
+                0.382 coin
+              </span>
             </div>
           </div>
+          <div>
+            <h3 className="font-semibold text-sm text-gray-400">BTC</h3>
+            <h1 className="font-semibold text-xl text-gray-700">$ 1,936.00</h1>
+          </div>
         </div>
-        </>
-    )
+      </div>
+    </>
+  );
 }
