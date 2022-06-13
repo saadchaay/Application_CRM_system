fc64fb9c98a9cabd51e0e6732ac5d90b9dfeff7d
Saad Chaay <93975374+saadchaay@users.noreply.github.com>
2022-06-13 23:23:12 +0100

    Update Integration.jsx

--- app/client/src/components/system/Integration.jsx
+++ app/client/src/components/system/Integration.jsx
@@ -1,25 +1,29 @@
 export default function Integration() {
   return (
     <>
-      <div className="flex-1 min-w-0 lg:border-t pt-4 ml-5">
-        <div className="bg-white rounded-3xl border shadow-xl p-8 w-60 sm:w-3/6">
+      <div className="flex-1 min-w-0 lg:border-t pt-4 mx-3">
+        <div className="bg-white rounded-3xl border shadow-xl p-8 w-full sm:w-3/6">
           <div className="flex justify-between items-center mb-4">
             <img
               src="https://cdn.neow.in/news/images/uploaded/2019/10/1570089797_google-sheets_story.jpg"
               alt=""
               className="w-32"
             />
-            <div>
-              <span className="font-bold text-green-500"> Linked Your app with Google sheet </span>
+            <div className="ml-1">
+              <span className="font-bold text-cyan-600">
+                {" "}
+                Linked Your app with Google sheet{" "}
+              </span>
               <br />
               <span className="font-medium text-xs text-gray-500 flex justify-end">
-                0.382 coin
+                Manage all your order with google sheets
               </span>
             </div>
           </div>
           <div>
-            <h3 className="font-semibold text-sm text-gray-400">BTC</h3>
-            <h1 className="font-semibold text-xl text-gray-700">$ 1,936.00</h1>
+            <button className="bg-cyan-600 hover:bg-cyan-700 text-white font-bold py-2 px-4 rounded-lg">
+              Connect Now
+            </button>
           </div>
         </div>
       </div>
