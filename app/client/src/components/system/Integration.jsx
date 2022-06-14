6d96bcf7b703bb5da617cc7be62107d3f69bdd39
Saad Chaay <93975374+saadchaay@users.noreply.github.com>
2022-06-14 15:13:49 +0100

    Update Integration.jsx

--- app/client/src/components/system/Integration.jsx
+++ app/client/src/components/system/Integration.jsx
@@ -201,79 +201,12 @@ export default function Integration() {
       </div>
 
       <div>
-        <div className="mt-8 flex flex-col">
-          <div className="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
-            <div className="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
-              <div className="overflow-hidden shadow ring-1 ring-black ring-opacity-5 md:rounded-lg">
-                <table className="min-w-full divide-y divide-gray-300">
-                  <thead className="bg-gray-50">
-                    <tr>
-                      <th
-                        scope="col"
-                        className="whitespace-nowrap py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6"
-                      >
-                        Sheet ID
-                      </th>
-                      <th
-                        scope="col"
-                        className="whitespace-nowrap px-2 py-3.5 text-left text-sm font-semibold text-gray-900"
-                      >
-                        Sheet Name
-                      </th>
-                      
-                      <th
-                        scope="col"
-                        className="whitespace-nowrap px-2 py-3.5 text-left text-sm font-semibold text-gray-900"
-                      >
-                        Sheet Link
-                      </th>
-                      <th
-                        scope="col"
-                        className="whitespace-nowrap px-2 py-3.5 text-left text-sm font-semibold text-gray-900"
-                      >
-                        Total order
-                      </th>
-                      <th
-                        scope="col"
-                        className="relative whitespace-nowrap py-3.5 pl-3 pr-4 sm:pr-6"
-                      >
-                        <span className="sr-only">Edit</span>
-                      </th>
-                    </tr>
-                  </thead>
-                  <tbody className="divide-y divide-gray-200 bg-white">
-                    {transactions.map((transaction) => (
-                      <tr key={transaction.id}>
-                        <td className="whitespace-nowrap py-2 pl-4 pr-3 text-sm text-gray-500 sm:pl-6">
-                          {transaction.id}
-                        </td>
-                        <td className="whitespace-nowrap px-2 py-2 text-sm font-medium text-gray-900">
-                          {transaction.customer}
-                        </td>
-                        
-                        <td className="whitespace-nowrap px-2 py-2 text-sm text-gray-500">
-                          {transaction.date}
-                        </td>
-
-                        <td className="whitespace-nowrap px-2 py-2 text-sm text-gray-500">
-                          {transaction.total}
-                        </td>
-                        <td className="relative whitespace-nowrap py-2 pl-3 pr-4 text-right text-sm font-medium sm:pr-6">
-                          <a
-                            href="/"
-                            className="text-indigo-600 hover:text-indigo-900"
-                          >
-                            details
-                            <span className="sr-only">, {transaction.id}</span>
-                          </a>
-                        </td>
-                      </tr>
-                    ))}
-                  </tbody>
-                </table>
-              </div>
-            </div>
-          </div>
+        <div className="mt-8 flex justify-end mx-6">
+          <button
+            className="text-white bg-cyan-700 hover:bg-cyan-800 rounded-md px-4 py-3"
+          >
+            Add SpreedSheet
+          </button>
         </div>
       </div>
 
