8bc28d03d07f2e5e6a3a9d5e5110bcc3ae9fbbb3
Saad Chaay <93975374+saadchaay@users.noreply.github.com>
2022-06-14 14:23:31 +0100

    Update Integration.jsx

--- app/client/src/components/system/Integration.jsx
+++ app/client/src/components/system/Integration.jsx
@@ -3,6 +3,19 @@ import axios from "../../api/axios";
 import { gapi } from "gapi-script";
 import { GoogleLogin } from "react-google-login";
 
+
+const transactions = [
+  {
+    id: "AAPS0L",
+    customer: "saad chaay",
+    phone: "+212615207417",
+    city: "Rabat",
+    date: "02/01/2020",
+    status: "Pending",
+    total: "$4,397.00",
+  },
+  // More transactions...
+];
 export default function Integration() {
   const auth = JSON.parse(localStorage.getItem("auth"));
   const [openForm, setOpenForm] = useState(false);
@@ -27,11 +40,10 @@ export default function Integration() {
         token: response.tokenId,
         clientId: data.clientId,
         clientSecret: data.clientSecret,
-      }
+      };
       setToken(data__);
       localStorage.setItem("token", JSON.stringify(data__));
     });
-    
   };
 
   const onFailure = (response) => {
@@ -56,14 +68,16 @@ export default function Integration() {
 
   const HandleDisconnect = (e) => {
     e.preventDefault();
-    axios.delete("ProfileController/deleteIntegration/" + auth.id).then((res) => {
-      console.log(res);
-      localStorage.removeItem("token");
-      setToken(null);
-      setData({});
-      setOpenForm(false);
-    });
-  }
+    axios
+      .delete("ProfileController/deleteIntegration/" + auth.id)
+      .then((res) => {
+        console.log(res);
+        localStorage.removeItem("token");
+        setToken(null);
+        setData({});
+        setOpenForm(false);
+      });
+  };
   return (
     <>
       <div className="flex-1 lg:border-t pt-4 mx-3">
@@ -86,118 +100,202 @@ export default function Integration() {
             </div>
           </div>
           <div>
-
-            {
-              (!token) ? 
-                  openForm ? (
-                  !data.clientId && !data.clientSecret ? (
+            {!token ? (
+              openForm ? (
+                !data.clientId && !data.clientSecret ? (
+                  <div>
                     <div>
-                      <div>
-                        <label
-                          htmlFor="first-name"
-                          className="block text-sm font-medium text-gray-700"
-                        >
-                          CLIENT ID
-                        </label>
-                        <input
-                          type="text"
-                          id="title"
-                          value={clientId}
-                          onChange={(e) => setClientId(e.target.value)}
-                          autoComplete="title"
-                          className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-gray-900 focus:border-gray-900 sm:text-sm"
-                        />
-                      </div>
-                      <div className="my-3">
-                        <label
-                          htmlFor="first-name"
-                          className="block text-sm font-medium text-gray-700"
-                        >
-                          CLIENT Secret
-                        </label>
-                        <input
-                          type="text"
-                          id="title"
-                          value={clientSecret}
-                          onChange={(e) => setClientSecret(e.target.value)}
-                          autoComplete="title"
-                          className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-gray-900 focus:border-gray-900 sm:text-sm"
-                        />
-                      </div>
-                      <div className=" flex justify-start mt-10">
-                        <button onClick={handleGoogle}>
-                          <span className="text-white bg-cyan-600 hover:bg-cyan-700 rounded-md px-4 py-3 ">
-                            Continue
-                          </span>
-                        </button>
-                        <button onClick={() => setOpenForm(false)} className="mx-2">
-                          <span className="text-gray-600 bg-white border hover:bg-gray-200 rounded-md px-4 py-3">
-                            Cancel
-                          </span>
-                        </button>
-                      </div>
+                      <label
+                        htmlFor="first-name"
+                        className="block text-sm font-medium text-gray-700"
+                      >
+                        CLIENT ID
+                      </label>
+                      <input
+                        type="text"
+                        id="title"
+                        value={clientId}
+                        onChange={(e) => setClientId(e.target.value)}
+                        autoComplete="title"
+                        className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-gray-900 focus:border-gray-900 sm:text-sm"
+                      />
                     </div>
-                  ) : (
-                    <div>
-                      <span className="text-cyan-600 text-md">
-                        Your Client ID and Client Secret Id has been added
-                      </span>
-                      <div className=" flex justify-start mt-4">
-                        <GoogleLogin
-                          render={(renderProps) => (
-                            <button className="text-white bg-cyan-500 hover:bg-cyan-700 py-2 px-3 rounded-lg" onClick={renderProps.onClick}>
-                              Connect Account
-                            </button>
-                          )}
-                          buttonText=""
-                          icon={false}
-                          clientId={data.clientId}
-                          onSuccess={onSuccess}
-                          onFailure={onFailure}
-                        />
-                      </div>
+                    <div className="my-3">
+                      <label
+                        htmlFor="first-name"
+                        className="block text-sm font-medium text-gray-700"
+                      >
+                        CLIENT Secret
+                      </label>
+                      <input
+                        type="text"
+                        id="title"
+                        value={clientSecret}
+                        onChange={(e) => setClientSecret(e.target.value)}
+                        autoComplete="title"
+                        className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-gray-900 focus:border-gray-900 sm:text-sm"
+                      />
                     </div>
-                  )
+                    <div className=" flex justify-start mt-10">
+                      <button onClick={handleGoogle}>
+                        <span className="text-white bg-cyan-600 hover:bg-cyan-700 rounded-md px-4 py-3 ">
+                          Continue
+                        </span>
+                      </button>
+                      <button
+                        onClick={() => setOpenForm(false)}
+                        className="mx-2"
+                      >
+                        <span className="text-gray-600 bg-white border hover:bg-gray-200 rounded-md px-4 py-3">
+                          Cancel
+                        </span>
+                      </button>
+                    </div>
+                  </div>
                 ) : (
-                  <button
-                    onClick={() => setOpenForm(true)}
-                    className="bg-cyan-600 hover:bg-cyan-700 text-white font-bold py-2 px-4 rounded-lg"
-                  >
-                    Connect Now
-                  </button>
-                )
-                : (
-                  <div className="flex flex-col">
-                    <div className="text-cyan-600 text-md">
-                      Your Account is connected with Google Sheets
+                  <div>
+                    <span className="text-cyan-600 text-md">
+                      Your Client ID and Client Secret Id has been added
+                    </span>
+                    <div className=" flex justify-start mt-4">
+                      <GoogleLogin
+                        render={(renderProps) => (
+                          <button
+                            className="text-white bg-cyan-500 hover:bg-cyan-700 py-2 px-3 rounded-lg"
+                            onClick={renderProps.onClick}
+                          >
+                            Connect Account
+                          </button>
+                        )}
+                        buttonText=""
+                        icon={false}
+                        clientId={data.clientId}
+                        onSuccess={onSuccess}
+                        onFailure={onFailure}
+                      />
                     </div>
-                    <button onClick={(e) => HandleDisconnect(e)} className="mt-3 w-36 text-white bg-red-700 hover:bg-red-800 rounded-md px-4 py-3">
-                      <span>
-                        Disconnect
-                      </span>
-                    </button>
-                </div>
+                  </div>
                 )
-            }
+              ) : (
+                <button
+                  onClick={() => setOpenForm(true)}
+                  className="bg-cyan-600 hover:bg-cyan-700 text-white font-bold py-2 px-4 rounded-lg"
+                >
+                  Connect Now
+                </button>
+              )
+            ) : (
+              <div className="flex flex-col">
+                <div className="text-cyan-600 text-md">
+                  Your Account is connected with Google Sheets
+                </div>
+                <button
+                  onClick={(e) => HandleDisconnect(e)}
+                  className="mt-3 w-36 text-white bg-red-700 hover:bg-red-800 rounded-md px-4 py-3"
+                >
+                  <span>Disconnect</span>
+                </button>
+              </div>
+            )}
           </div>
         </div>
       </div>
 
       <div>
-        <div className="flex-1 pt-4 mx-3">
-          <div className="bg-white rounded-3xl border shadow-xl p-8 w-full sm:w-3/6">
-            <div className="flex justify-between items-center mb-4">
-              <img
-                src="https://cdn.neow.in/news/images/uploaded/2019/10/1570089797_google-sheets_story.jpg"
-                alt=""
-                className="w-32"
-              />
-              <div className="ml-1">
-                <span className="font-bold text-cyan-600">
-                  {" "}
-                  Linked Your app with Google sheet{" "}
-                </span>
-                <br />
+        <div className="mt-8 flex flex-col">
+          <div className="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
+            <div className="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
+              <div className="overflow-hidden shadow ring-1 ring-black ring-opacity-5 md:rounded-lg">
+                <table className="min-w-full divide-y divide-gray-300">
+                  <thead className="bg-gray-50">
+                    <tr>
+                      <th
+                        scope="col"
+                        className="whitespace-nowrap py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6"
+                      >
+                        Order ID
+                      </th>
+                      <th
+                        scope="col"
+                        className="whitespace-nowrap px-2 py-3.5 text-left text-sm font-semibold text-gray-900"
+                      >
+                        Customer
+                      </th>
+                      <th
+                        scope="col"
+                        className="whitespace-nowrap px-2 py-3.5 text-left text-sm font-semibold text-gray-900"
+                      >
+                        Phone
+                      </th>
+                      <th
+                        scope="col"
+                        className="whitespace-nowrap px-2 py-3.5 text-left text-sm font-semibold text-gray-900"
+                      >
+                        City
+                      </th>
+                      <th
+                        scope="col"
+                        className="whitespace-nowrap px-2 py-3.5 text-left text-sm font-semibold text-gray-900"
+                      >
+                        Date
+                      </th>
+                      <th
+                        scope="col"
+                        className="whitespace-nowrap px-2 py-3.5 text-left text-sm font-semibold text-gray-900"
+                      >
+                        Status
+                      </th>
+                      <th
+                        scope="col"
+                        className="whitespace-nowrap px-2 py-3.5 text-left text-sm font-semibold text-gray-900"
+                      >
+                        Total order
+                      </th>
+                      <th
+                        scope="col"
+                        className="relative whitespace-nowrap py-3.5 pl-3 pr-4 sm:pr-6"
+                      >
+                        <span className="sr-only">Edit</span>
+                      </th>
+                    </tr>
+                  </thead>
+                  <tbody className="divide-y divide-gray-200 bg-white">
+                    {transactions.map((transaction) => (
+                      <tr key={transaction.id}>
+                        <td className="whitespace-nowrap py-2 pl-4 pr-3 text-sm text-gray-500 sm:pl-6">
+                          {transaction.id}
+                        </td>
+                        <td className="whitespace-nowrap px-2 py-2 text-sm font-medium text-gray-900">
+                          {transaction.customer}
+                        </td>
+                        <td className="whitespace-nowrap px -2 py-2 text-sm text-gray-900">
+                          {transaction.phone}
+                        </td>
+                        <td className="whitespace-nowrap px-2 py-2 text-sm text-gray-500">
+                          {transaction.city}
+                        </td>
+                        <td className="whitespace-nowrap px-2 py-2 text-sm text-gray-500">
+                          {transaction.date}
+                        </td>
+                        <td className="whitespace-nowrap px-2 py-2 text-sm text-gray-500">
+                          {transaction.status}
+                        </td>
+                        <td className="whitespace-nowrap px-2 py-2 text-sm text-gray-500">
+                          {transaction.total}
+                        </td>
+                        <td className="relative whitespace-nowrap py-2 pl-3 pr-4 text-right text-sm font-medium sm:pr-6">
+                          <a
+                            href="/"
+                            className="text-indigo-600 hover:text-indigo-900"
+                          >
+                            details
+                            <span className="sr-only">, {transaction.id}</span>
+                          </a>
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
               </div>
             </div>
           </div>
