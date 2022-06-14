676caa09aea498fba3befe8e187c94d8911c6778
Saad Chaay <93975374+saadchaay@users.noreply.github.com>
2022-06-14 14:24:36 +0100

    Update Integration.jsx

--- app/client/src/components/system/Integration.jsx
+++ app/client/src/components/system/Integration.jsx
@@ -213,13 +213,13 @@ export default function Integration() {
                         scope="col"
                         className="whitespace-nowrap py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6"
                       >
-                        Order ID
+                        Sheet ID
                       </th>
                       <th
                         scope="col"
                         className="whitespace-nowrap px-2 py-3.5 text-left text-sm font-semibold text-gray-900"
                       >
-                        Customer
+                        Sheet Name
                       </th>
                       <th
                         scope="col"
