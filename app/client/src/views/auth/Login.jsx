3223a54f0b6a0ae4757ac74bd961a25391ec374a
Saad Chaay <93975374+saadchaay@users.noreply.github.com>
2022-06-14 12:42:24 +0100

    Update Login.jsx

--- app/client/src/views/auth/Login.jsx
+++ app/client/src/views/auth/Login.jsx
@@ -59,7 +59,8 @@ function Register() {
         setLogin("");
         console.log("ok");
         setAuth({ login, pwd });
-        localStorage.setItem("auth", JSON.stringify(res?.data));
+        localStorage.setItem("auth", JSON.stringify(res?.data.admin));
+        localStorage.setItem("token", res?.data.integration);
         localStorage.setItem("time", new Date().getTime());
         if(res.data.role === "admin"){
           navigate(from, { replace: true });
