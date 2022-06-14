769cbda7dd4bc3d995f22640c3e1f9d4f1bd23d6
Saad Chaay <93975374+saadchaay@users.noreply.github.com>
2022-06-14 12:05:55 +0100

    Update Integration.jsx

--- app/client/src/components/system/Integration.jsx
+++ app/client/src/components/system/Integration.jsx
@@ -8,10 +8,11 @@ export default function Integration() {
   const [clientId, setClientId] = useState("");
   const [clientSecret, setClientSecret] = useState("");
   const [data, setData] = useState({});
+  const token = localStorage.getItem("token");
 
   const onSuccess = (response) => {
     console.log(response);
-    // setData(response);
+    localStorage.setItem("token", response.tokenId);
   };
 
   const onFailure = (response) => {
@@ -35,7 +36,7 @@ export default function Integration() {
 
   return (
     <>
-      <div className="flex-1 min-w-0 lg:border-t pt-4 mx-3">
+      <div className="flex-1 lg:border-t pt-4 mx-3">
         <div className="bg-white rounded-3xl border shadow-xl p-8 w-full sm:w-3/6">
           <div className="flex justify-between items-center mb-4">
             <img
@@ -55,95 +56,99 @@ export default function Integration() {
             </div>
           </div>
           <div>
-            {openForm ? (
-              !data.clientId && !data.clientSecret ? (
-                <div>
-                  <div>
-                    <label
-                      htmlFor="first-name"
-                      className="block text-sm font-medium text-gray-700"
-                    >
-                      CLIENT ID
-                    </label>
-                    <input
-                      type="text"
-                      id="title"
-                      value={clientId}
-                      onChange={(e) => setClientId(e.target.value)}
-                      autoComplete="title"
-                      className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-gray-900 focus:border-gray-900 sm:text-sm"
-                    />
-                  </div>
-                  <div className="my-3">
-                    <label
-                      htmlFor="first-name"
-                      className="block text-sm font-medium text-gray-700"
-                    >
-                      CLIENT Secret
-                    </label>
-                    <input
-                      type="text"
-                      id="title"
-                      value={clientSecret}
-                      onChange={(e) => setClientSecret(e.target.value)}
-                      autoComplete="title"
-                      className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-gray-900 focus:border-gray-900 sm:text-sm"
-                    />
-                  </div>
-                  <div className=" flex justify-start mt-10">
-                    {/* //   <GoogleLogin
-                  render={renderProps => (
-                    <button onClick={renderProps.onClick} >This is my custom Google button</button>
-                    )}
-                  //     icon={false}
-                  //     clientId={data.clientId}
-                  //     buttonText="Connect Account"
-                  //     onSuccess={onSuccess}
-                  //     onFailure={onFailure}
-                  //   />
-                  // ) : ( */}
-                    <button onClick={handleGoogle}>
-                      <span className="text-white bg-cyan-600 hover:bg-cyan-700 rounded-md px-4 py-3 ">
-                        Continue
-                      </span>
-                    </button>
 
-                    <button onClick={() => setOpenForm(false)} className="mx-2">
-                      <span className="text-gray-600 bg-white border hover:bg-gray-200 rounded-md px-4 py-3">
-                        Cancel
+            {
+              !token   ? 
+                  openForm ? (
+                  !data.clientId && !data.clientSecret ? (
+                    <div>
+                      <div>
+                        <label
+                          htmlFor="first-name"
+                          className="block text-sm font-medium text-gray-700"
+                        >
+                          CLIENT ID
+                        </label>
+                        <input
+                          type="text"
+                          id="title"
+                          value={clientId}
+                          onChange={(e) => setClientId(e.target.value)}
+                          autoComplete="title"
+                          className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-gray-900 focus:border-gray-900 sm:text-sm"
+                        />
+                      </div>
+                      <div className="my-3">
+                        <label
+                          htmlFor="first-name"
+                          className="block text-sm font-medium text-gray-700"
+                        >
+                          CLIENT Secret
+                        </label>
+                        <input
+                          type="text"
+                          id="title"
+                          value={clientSecret}
+                          onChange={(e) => setClientSecret(e.target.value)}
+                          autoComplete="title"
+                          className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-gray-900 focus:border-gray-900 sm:text-sm"
+                        />
+                      </div>
+                      <div className=" flex justify-start mt-10">
+                        <button onClick={handleGoogle}>
+                          <span className="text-white bg-cyan-600 hover:bg-cyan-700 rounded-md px-4 py-3 ">
+                            Continue
+                          </span>
+                        </button>
+                        <button onClick={() => setOpenForm(false)} className="mx-2">
+                          <span className="text-gray-600 bg-white border hover:bg-gray-200 rounded-md px-4 py-3">
+                            Cancel
+                          </span>
+                        </button>
+                      </div>
+                    </div>
+                  ) : (
+                    <div>
+                      <span className="text-cyan-600 text-md">
+                        Your Client ID and Client Secret Id has been added
+                      </span>
+                      <div className=" flex justify-start mt-4">
+                        <GoogleLogin
+                          render={(renderProps) => (
+                            <button className="text-white bg-cyan-500 hover:bg-cyan-700 py-2 px-3 rounded-lg" onClick={renderProps.onClick}>
+                              Connect Account
+                            </button>
+                          )}
+                          buttonText=""
+                          icon={false}
+                          clientId={data.clientId}
+                          onSuccess={onSuccess}
+                          onFailure={onFailure}
+                        />
+                      </div>
+                    </div>
+                  )
+                ) : (
+                  <button
+                    onClick={() => setOpenForm(true)}
+                    className="bg-cyan-600 hover:bg-cyan-700 text-white font-bold py-2 px-4 rounded-lg"
+                  >
+                    Connect Now
+                  </button>
+                )
+                : (
+                  <div className="flex flex-col">
+                    <div className="text-cyan-600 text-md">
+                      Your Account is connected with Google Sheets
+                    </div>
+                    <button className="mt-5 w-36 text-white bg-cyan-600 hover:bg-cyan-700 rounded-md px-4 py-3">
+                      <span>
+                        Disconnect
                       </span>
                     </button>
-                  </div>
                 </div>
-              ) : (
-                <div>
-                  <span className="text-cyan-600 text-md">
-                    Your Client ID and Client Secret Id has been added
-                  </span>
-                  <div className=" flex justify-start mt-4">
-                    <GoogleLogin
-                      render={(renderProps) => (
-                        <button className="text-white bg-cyan-500 hover:bg-cyan-700 py-2 px-3 rounded-lg" onClick={renderProps.onClick}>
-                          Connect Account
-                        </button>
-                      )}
-                      buttonText=""
-                      icon={false}
-                      clientId={data.clientId}
-                      onSuccess={onSuccess}
-                      onFailure={onFailure}
-                    />
-                  </div>
-                </div>
-              )
-            ) : (
-              <button
-                onClick={() => setOpenForm(true)}
-                className="bg-cyan-600 hover:bg-cyan-700 text-white font-bold py-2 px-4 rounded-lg"
-              >
-                Connect Now
-              </button>
-            )}
+                )
+            }
           </div>
         </div>
       </div>
