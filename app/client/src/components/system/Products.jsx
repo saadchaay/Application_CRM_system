9bbf98394a3ba178661bf55147e8c9034cbf11d1
Saad Chaay <93975374+saadchaay@users.noreply.github.com>
2022-06-17 09:21:51 +0100

    hgsjsn

--- app/client/src/components/system/Products.jsx
+++ app/client/src/components/system/Products.jsx
@@ -208,6 +208,17 @@ export default function Example() {
               category: res.data.category,
             });
           }
+          if(res.data.sku){
+            setErrors({
+              ...errors,
+              title: res.data.title,
+              description: res.data.description,
+              quantity: res.data.quantity,
+              price: res.data.price,
+              category: res.data.category,
+              sku: res.data.sku,
+            });
+          }
         }
       }
     }
@@ -613,7 +624,7 @@ export default function Example() {
                     scope="col"
                     className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6"
                   >
-                    Number
+                    SKU product
                   </th>
                   <th
                     scope="col"
@@ -657,7 +668,7 @@ export default function Example() {
                 {products.map((item, index) => (
                   <tr key={item.id}>
                     <td className="w-full max-w-0 py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:w-auto sm:max-w-none sm:pl-6">
-                      <Link to={`/product/${item.id}`}> <span className="underline text-cyan-600 font-bold">## {item.id}</span> </Link>
+                      <Link to={`/product/${item.id}`}> <span className="text-cyan-600 font-bold">{item.sku}</span> </Link>
                       <dl className="font-normal lg:hidden">
                         <dt className="sr-only">Category</dt>
                         <dd className="mt-1 truncate text-gray-700">
