54ed0db1f2d893116950ab26f700cc295b88c0ef
Saad Chaay <93975374+saadchaay@users.noreply.github.com>
2022-06-17 16:50:50 +0100

    Update Orders.jsx

--- app/client/src/components/system/Orders.jsx
+++ app/client/src/components/system/Orders.jsx
@@ -27,17 +27,22 @@ export default function Orders() {
   const [orders, setOrders] = useState(null);
   const [success, setSuccess] = useState(null);
   const [error, setError] = useState(null);
+  const [spreadsheet, setSpreadsheet] = useState([]);
+  const [sheet, setSheet] = useState("");
 
   const getFiles = async () => {
-    const res = await axios.get("")
-  }
+    const res = await axios.get("SheetsController/index/" + auth.id);
+    setSpreadsheet(res.data);
+  };
 
   const handleOrderFromSheet = async (e) => {
     e.preventDefault();
+    const sheetID = sheet;
+    console.log(sheetID);
     var accessToken = gapi.auth.getToken().access_token;
     const object = {};
     const response = await fetch(
-      "https://sheets.googleapis.com/v4/spreadsheets/1W4sXanZLLuHi3xjobdtDuXToGnFnPWLVzAHuZP-qCVE/values/A1:Z1000",
+      "https://sheets.googleapis.com/v4/spreadsheets/"+ sheetID +"/values/A1:Z1000",
       {
         method: "GET",
         headers: new Headers({ Authorization: "Bearer " + accessToken }),
@@ -87,7 +92,7 @@ export default function Orders() {
               newErr.message = res.data.message;
             } else if (res.data.orderErr) {
               newErr.orderErr = res.data.orderErr;
-            } else if(res.data.productErr){
+            } else if (res.data.productErr) {
               newErr.productErr = res.data.productErr;
             }
             setError(newErr);
@@ -114,6 +119,7 @@ export default function Orders() {
   };
 
   useEffect(() => {
+    getFiles();
     function start() {
       gapi.client.init({
         apiKey: token.apiKey ? token.apiKey : API_KEY,
@@ -135,29 +141,44 @@ export default function Orders() {
           <div className="sm:flex-auto">
             <h1 className="text-xl font-semibold text-gray-900">Orders</h1>
             <p className="mt-2 text-sm text-gray-700">
-              A table of placeholder stock market data that does not make any
-              sense.
+              A table of all your orders ..., you can also import your orders to our system from a google sheet.
             </p>
           </div>
-          <div className="mt-4 sm:mt-0 sm:ml-16 sm:flex-none">
+          <div className="mt-4 flex flex-col sm:mt-0 sm:ml-16 sm:flex-none">
+            <select
+              id="role"
+              value={sheet}
+              onChange={(e) => setSheet(e.target.value)}
+              autoComplete="country-name"
+              className="mt-1 block w-full bg-white border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-gray-900 focus:border-gray-900 sm:text-sm"
+            >
+              <option>Choose google sheet file</option>
+              {spreadsheet.map((sheet) => (
+                <option key={sheet.id} value={sheet.spreadsheetId}>
+                  {sheet.fileName}
+                </option>
+              ))}
+            </select>
             <button
               type="button"
               onClick={handleOrderFromSheet}
-              className="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto"
+              className="mt-2 inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto"
             >
-              Import Order From Google Sheet
+              Import Orders
             </button>
           </div>
         </div>
-        {
-          error ?
-          ( error.message ? (
+        {error ? (
+          error.message ? (
             <div
               className="mt-2 bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative"
               role="alert"
             >
               <strong className="font-bold">Failed!</strong>
-              <span className="ml-2 block sm:inline">Orders Not Imported {error.messaage ? ", " + error.messaage : null} </span>
+              <span className="ml-2 block sm:inline">
+                Orders Not Imported{" "}
+                {error.messaage ? ", " + error.messaage : null}{" "}
+              </span>
               <span
                 className="absolute top-0 bottom-0 right-0 px-4 py-3"
                 onClick={() => setError(null)}
@@ -180,7 +201,9 @@ export default function Orders() {
             >
               <strong className="font-bold">Warning!</strong>
               <span className="ml-2 block sm:inline">
-                Some orders is already exists {error.orderErr ? ", " + error.orderErr : null} {error.productErr ? ", "+error.productErr : null} 
+                Some orders is already exists{" "}
+                {error.orderErr ? ", " + error.orderErr : null}{" "}
+                {error.productErr ? ", " + error.productErr : null}
               </span>
               <span
                 className="absolute top-0 bottom-0 right-0 px-4 py-3"
@@ -197,34 +220,33 @@ export default function Orders() {
                 </svg>
               </span>
             </div>
-          ) : null ) : null
-        } 
-        {
-          success ? (
-            <div
-              className="mt-2 bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative"
-              role="alert"
+          ) : null
+        ) : null}
+        {success ? (
+          <div
+            className="mt-2 bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative"
+            role="alert"
+          >
+            <strong className="font-bold">Success!</strong>
+            <span className="ml-2 block sm:inline">
+              Imported all orders successfully
+            </span>
+            <span
+              className="absolute top-0 bottom-0 right-0 px-4 py-3"
+              onClick={() => setSuccess(false)}
             >
-              <strong className="font-bold">Success!</strong>
-              <span className="ml-2 block sm:inline">
-                Imported all orders successfully
-              </span>
-              <span
-                className="absolute top-0 bottom-0 right-0 px-4 py-3"
-                onClick={() => setSuccess(false)}
+              <svg
+                className="fill-current h-6 w-6 text-green-500"
+                role="button"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
               >
-                <svg
-                  className="fill-current h-6 w-6 text-green-500"
-                  role="button"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
-                >
-                  <title>Close</title>
-                  <path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" />
-                </svg>
-              </span>
-            </div>) : null
-        }
+                <title>Close</title>
+                <path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" />
+              </svg>
+            </span>
+          </div>
+        ) : null}
         <div className="mt-8 flex flex-col">
           <div className="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
             <div className="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
